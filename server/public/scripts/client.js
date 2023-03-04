70637c446c52233d60277cc72fc1c6d2982431f7
MIguel Torres <mtorres913@gmail.com>
2023-03-04 17:44:34 -0600

    working on submitKoala function

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,5 +1,18 @@
 console.log( 'js' );
 
+let nameIn = document.querySelector('#name').value
+let ageIn = document.querySelector('#ageIn').value
+let genderIn = document.querySelector('#genderIn').value
+let readyForTransferIn = document.querySelector('#readyForTransferIn').value
+let notesIn = document.querySelector('#notesIn').value
+
+function submitKoala(event)
+let koalasForServer = {
+  name: nameIn,
+  age: ageIn,
+  gender: genderIn,
+  notes: notesIn,
+};
 function getKoalas(){
   console.log( 'in getKoalas' );
   // axios call to server to get koalas
