ea7131fd8755be0d160136bd4cfa835f8ea1381d
MIguel Torres <mtorres913@gmail.com>
2023-03-05 14:27:01 -0600

    finished filling out koalalist on koala router js

--- server/routes/koala.router.js
+++ server/routes/koala.router.js
@@ -2,10 +2,12 @@ const express = require('express');
 const koalaRouter = express.Router();
 
 let koalaList = [
-    {id: 1, name: 'Scotty', gender:'M', age:'4', ready_to_transfer: 'Y', notes: ''}
-
-
-
+    {id: 1, name: 'Scotty', gender:'M', age:'4', ready_to_transfer: 'Y', notes: 'Born in Guatemala'},
+    {id: 2, name: 'Jean', gender:'F', age:'5', ready_to_transfer: 'Y', notes: 'Allergic to lots of lava'},
+    {id: 3, name: 'Ororo', gender:'F', age:'7', ready_to_transfer: 'N', notes: 'Loves listening to Paula (Abdul)'},
+    {id: 4, name: 'Logan', gender:'M', age:'15', ready_to_transfer: 'N', notes: 'Loves the sauna'},
+    {id: 5, name: 'Charlie', gender:'M', age:'9', ready_to_transfer: 'Y', notes: 'Favorite band is Nirvana'},
+    {id: 6, name: 'Betsy', gender:'F', age:'4', ready_to_transfer: 'Y', notes: 'Has a pet iguana'},
 ]
 // DB CONNECTION
 
