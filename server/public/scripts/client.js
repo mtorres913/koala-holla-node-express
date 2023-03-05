7ffc08096d0c63676f458bc58834e9cb0284d772
MIguel Torres <mtorres913@gmail.com>
2023-03-04 18:46:31 -0600

    finished submitKoala function

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -6,13 +6,25 @@ let genderIn = document.querySelector('#genderIn').value
 let readyForTransferIn = document.querySelector('#readyForTransferIn').value
 let notesIn = document.querySelector('#notesIn').value
 
-function submitKoala(event)
+function submitKoala(event){
+event.preventDefault();
+console.log('In submitKoala function');
 let koalasForServer = {
   name: nameIn,
   age: ageIn,
   gender: genderIn,
   notes: notesIn,
 };
+axios.post('/koalas', koalasForServer).then((response) => {
+  console.log(response);
+  getKoalas();
+  document.querySelector('#addKoala').reset()
+}).catch((error) => {
+  console.log(error);
+  alert('Something went wrong.');
+});
+
+}
 function getKoalas(){
   console.log( 'in getKoalas' );
   // axios call to server to get koalas
--- server/routes/koala.router.js
+++ server/routes/koala.router.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const koalaRouter = express.Router();
 
+let koalaList = []
 // DB CONNECTION
 
 
@@ -8,7 +9,15 @@ const koalaRouter = express.Router();
 
 
 // POST
-
+router.post('/', (req, res) => {
+    console.log('POST Request made for /koalas');
+    // Any data we send from the client is available
+    // as a property of req.body.
+    console.log(req.body);
+    let koalaToAdd = req.body;
+    koalaList.push(koalaToAdd);
+    res.sendStatus(201); // Success!
+});
 
 // PUT
 
