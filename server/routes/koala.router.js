5c975b0d72f0fafdf21ffe854f74134f6db2f9a8
MIguel Torres <mtorres913@gmail.com>
2023-03-11 18:43:47 -0600

    configured koalarouter.get in koala.router.js

--- server/routes/koala.router.js
+++ server/routes/koala.router.js
@@ -2,21 +2,29 @@ const express = require('express');
 const koalaRouter = express.Router();
 
 let koalaList = [
-    {id: 1, name: 'Scotty', gender:'M', age:'4', ready_to_transfer: 'Y', notes: 'Born in Guatemala'},
-    {id: 2, name: 'Jean', gender:'F', age:'5', ready_to_transfer: 'Y', notes: 'Allergic to lots of lava'},
-    {id: 3, name: 'Ororo', gender:'F', age:'7', ready_to_transfer: 'N', notes: 'Loves listening to Paula (Abdul)'},
-    {id: 4, name: 'Logan', gender:'M', age:'15', ready_to_transfer: 'N', notes: 'Loves the sauna'},
-    {id: 5, name: 'Charlie', gender:'M', age:'9', ready_to_transfer: 'Y', notes: 'Favorite band is Nirvana'},
-    {id: 6, name: 'Betsy', gender:'F', age:'4', ready_to_transfer: 'Y', notes: 'Has a pet iguana'},
+    // {id: 1, name: 'Scotty', gender:'M', age:'4', ready_to_transfer: 'Y', notes: 'Born in Guatemala'},
+    // {id: 2, name: 'Jean', gender:'F', age:'5', ready_to_transfer: 'Y', notes: 'Allergic to lots of lava'},
+    // {id: 3, name: 'Ororo', gender:'F', age:'7', ready_to_transfer: 'N', notes: 'Loves listening to Paula (Abdul)'},
+    // {id: 4, name: 'Logan', gender:'M', age:'15', ready_to_transfer: 'N', notes: 'Loves the sauna'},
+    // {id: 5, name: 'Charlie', gender:'M', age:'9', ready_to_transfer: 'Y', notes: 'Favorite band is Nirvana'},
+    // {id: 6, name: 'Betsy', gender:'F', age:'4', ready_to_transfer: 'Y', notes: 'Has a pet iguana'},
 ]
 // DB CONNECTION
 
 
 // GET
 koalaRouter.get('/', (req, res) => {
-    console.log('GET Request made for /quotes');
-    // Send back the list of quotes!
-    res.send(koalaList);
+    console.log('GET Request made for /koalas');
+    // Send back the list of koalas!
+    let queryText = 'SELECT * FROM "koalas";';
+    pool.query(queryText).then((result) => {
+        //result.rows is the Array of data from our database
+        res.send(result.rows);
+    }).catch((error) => {
+        console.log(`Error in GET ${error}`)
+     // Tell the client something went wrong
+     res.sendStatus(500);
+    })    
 });
 // POST
 koalaRouter.post('/', (req, res) => {
